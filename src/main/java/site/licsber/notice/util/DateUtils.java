8d700b440b740e67fef8868e13a4c71a7a280279
licsber <silverwings@foxmail.com>
2020-05-22 00:35:53 +0800

    change DateUtils SimpleDateFormatter to DateTimeFormatter

--- src/main/java/site/licsber/notice/util/DateUtils.java
+++ src/main/java/site/licsber/notice/util/DateUtils.java
@@ -1,20 +1,36 @@
 package site.licsber.notice.util;
 
-import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateUtils {
 
-    public static String getTimestamp(Date date) {
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+    private static final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss",
+                    Locale.CHINA).withZone(ZoneId.of("Asia/Shanghai"));
+
+    public static String getTimestamp(Instant instant) {
+        return formatter.format(instant);
     }
 
-    public static String getTimestamp(long timestamp) {
-        return getTimestamp(new Date(timestamp * 1000));
+    public static String getTimestamp(Date date) {
+        // https://stackoverflow.com/questions/25376242/java8-java-util-date-conversion-to-java-time-zoneddatetime
+        return getTimestamp(date.toInstant());
+    }
+
+    public static String getTimestampSec(long timestampSec) {
+        return getTimestamp(Instant.ofEpochSecond(timestampSec));
+    }
+
+    public static String getTimestampMil(long timestampMil) {
+        return getTimestamp(Instant.ofEpochMilli(timestampMil));
     }
 
     public static String getNowTimestamp() {
-        return getTimestamp(new Date());
+        return getTimestamp(Instant.now());
     }
 
 }
--- src/test/java/site/licsber/notice/util/DateUtilsTests.java
+++ src/test/java/site/licsber/notice/util/DateUtilsTests.java
@@ -7,7 +7,8 @@ public class DateUtilsTests {
 
     @Test
     void getTimestamp() {
-        Assertions.assertEquals("2020-05-19 11:34:09", DateUtils.getTimestamp(1589859249));
+        Assertions.assertEquals("2020-05-19 11:34:09", DateUtils.getTimestampSec(1589859249));
+        Assertions.assertEquals("2020-05-19 11:34:09", DateUtils.getTimestampMil(1589859249000L));
     }
 
     @Test
